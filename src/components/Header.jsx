adeab7ac265a93587082c9c6c24808a95fd4f6ab
Ravi Gupta <rg1729rg@gmail.com>
2024-03-06 23:52:54 +0530

    router and rating

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,24 +2,31 @@ import React from 'react'
 import { Navbar, Nav, Container } from 'react-bootstrap'
 import { FaShoppingCart, FaUser } from 'react-icons/fa'
 import logo from '../assets/logo.png'
+import { LinkContainer } from 'react-router-bootstrap'
 
 const Header = () => {
   return (
     <div>
         <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
-              <Container>
-                  <Navbar.Brand href="/">
-                      <img src={logo} alt='Proshop' />
-                      ProShop
-                  </Navbar.Brand>
-                  <Navbar.Toggle area-controls="basic-navbar-nav" />
-                  <Navbar.Collapse id="basic-navbar-nav">
-                      <Nav className='ms-auto'>
-                          <Nav.Link href='/cart'><FaShoppingCart /> Cart</Nav.Link>
-                          <Nav.Link href='/login'><FaUser /> Sign In</Nav.Link>
-                      </Nav>
-                  </Navbar.Collapse>
-              </Container> 
+            <Container>
+                <LinkContainer to='/'>
+                    <Navbar.Brand href="/">
+                        <img src={logo} alt='Proshop' />
+                        ProShop
+                    </Navbar.Brand>
+                </LinkContainer>
+                <Navbar.Toggle area-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className='ms-auto'>
+                        <LinkContainer to='/cart'>
+                              <Nav.Link href='/cart'><FaShoppingCart /> Cart</Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to='/login'>
+                            <Nav.Link href='/login'><FaUser /> Sign In</Nav.Link>
+                        </LinkContainer>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container> 
         </Navbar>
     </div>
   )
